f12d97d936be76bd2795f5a17b4d8e6c29ebd614
codeaktivist <codeaktivist@gmail.com>
2022-03-27 08:39:35 +0200

    Standard header implemented
    Not using structs, bext chunk tested

--- wav-audio-worker.c
+++ wav-audio-worker.c
@@ -1,6 +1,127 @@
-#include <stdio.c>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
-int main (void)
+// Aliases for primitive data types
+typedef uint8_t  BYTE;
+typedef uint32_t DWORD;
+typedef int32_t  LONG;
+typedef uint16_t WORD;
+
+typedef struct
 {
-    // soon
+    DWORD ckID;
+    DWORD ckSize;
+    //BYTE ckData[ckSize];
+} broadcast_audio_extension;
+
+int main(int argc, char *argv[])
+{
+    // Open audiofile
+    char *filepath = "Glas.wav";
+    FILE *audiofile = fopen(filepath, "r");
+    if (audiofile == NULL)
+    {
+        printf("Could not open file %s\n", filepath);
+        return 1;
+    }
+
+    // Read and check file header
+
+    // RIFF WAVE Chunk (mandatory)
+    // http://www-mmsp.ece.mcgill.ca/Documents/AudioFormats/WAVE/Docs/riffmci.pdf
+    // https://tech.ebu.ch/docs/tech/tech3285.pdf
+
+    // Check groupID for "RIFF"
+    char groupID[5];
+    groupID[4] = '\0';
+    fread(&groupID, sizeof(char), 4, audiofile);
+    if (strcmp(groupID, "RIFF\0") != 0)
+    {
+        printf("Only RIFF-WAVE-Files are supported!\n");
+        printf("Chunk ID:   %s\n", groupID);
+        return 2;    
+    }
+    printf("Chunk ID:   %s\n", groupID);
+
+    // Check fileSize, chunkSize = file length in bytes - 8
+    uint32_t fileSize;
+    fread(&fileSize, sizeof(uint32_t), 1, audiofile);
+    printf("File Size:  %u kb\n", (fileSize + 8) / 1024);
+
+    // Check riffType for "WAVE"
+    char riffType[5];
+    riffType[4] = '\0';
+    fread(&riffType, sizeof(char), 4, audiofile);
+    if (strcmp(riffType, "WAVE\0") != 0)
+    {
+        printf("Only RIFF-WAVE-Files are supported!\n");
+        printf("Riff Type:  %s\n", riffType);
+        return 2;    
+    }
+    printf("Riff Type:  %s\n", riffType);
+
+    // Chunks are defined by ckID and ckSize followed by the ckData with a length of ckSize
+    // Consume Chunk, analyse chID and call according reader function
+
+
+
+    // char chunkID[5];
+    // chunkID[4] = '\0';
+    // fread(&chunkID, sizeof(char), 4, audiofile);
+    // if (strcmp(chunkID, "bext") == 0)
+    // {
+    //     // Bext Chunk for Broadcast Wave Format
+    //     printf("\nBroadcast Wave Format Information:\n\n");
+    // }
+    // else if (strcmp(chunkID, "fmt ") == 0)
+    // {
+    //     printf("\nWave Header Information:\n\n");
+    // }
+    // printf("%s", chunkID);
+    // return 0;
+
+
+    // // Bext Chunk for Broadcast Wave Format
+    // char bextChunk[5];
+
+
+    // Format Chunk
+
+    // Check formatChunk for "fmt "
+    char formatChunk[5];
+    formatChunk[4] = '\0';
+    fread(formatChunk, sizeof(char), 4, audiofile);
+    if (strcmp(formatChunk, "fmt ") != 0)
+    {
+        printf("Wrong format-chunk found (fmt ): %s\n", formatChunk);
+        return 2;
+    }
+    printf("Format ID:  %s\n", formatChunk);
+
+    // Check format length
+    uint16_t fmtlength;
+    fread(&fmtlength, sizeof(uint16_t), 1, audiofile);
+    printf("fmt length: %i Bytes\n", fmtlength);
+
+    // Check format tag: 0x0001 -> PCM
+    uint16_t wFormatTag;
+    fread(&wFormatTag, sizeof(uint16_t), 1, audiofile);
+    if (wFormatTag != 0)
+    {
+        printf("Only PCM files are supported: wFormatTag = %i\n", wFormatTag);
+        return 2;
+    }
+    printf("Format Tag: %i (PCM)\n", wFormatTag);
+
+    // Get audio channels
+    uint16_t wChannels;
+    fread(&wChannels, sizeof(uint16_t), 1, audiofile);
+    printf("Audio channels: %i\n", wChannels);
+
+    // Get audio channels
+    uint32_t dwSamplesPerSec;
+    fread(&dwSamplesPerSec, sizeof(uint32_t), 1, audiofile);
+    printf("Samplerate: %i\n", dwSamplesPerSec);
+    
 }
