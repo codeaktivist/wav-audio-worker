fda5555436f11e4bd77fb5266c2aae79627ed152
codeaktivist <codeaktivist@gmail.com>
2022-03-29 11:52:00 +0200

    Added chunk IDs, formatting output, update readme

--- README.md
+++ README.md
@@ -22,12 +22,39 @@ Only uncompressed wave files are supported, meaning linear PCM files in a wav-co
 
 ## Background
 
-What's under the hood and what specs are used
+What's under the hood and what specs are used:
+
+[WAVE Header Reference](http://www-mmsp.ece.mcgill.ca/Documents/AudioFormats/WAVE/Docs/riffmci.pdf)
+
+[Broadcast WAVE Header Reference](https://tech.ebu.ch/docs/tech/tech3285.pdf)
+
+[Information on Chunks](https://www2.ak.tu-berlin.de/~fhein/Alias/Studio/ProTools/audio-formate/wav/overview.html)
 
 ### Analyzing the header info and metadata
 
 To make sense of the 0s and 1s in audio data, you need to know how many channels there are and what the bit depth and sample rate are. This information is provided in the file header.
 
-[WAVE Header Reference](http://www-mmsp.ece.mcgill.ca/Documents/AudioFormats/WAVE/Docs/riffmci.pdf)
+## Supported chunks
 
-[Broadcast WAVE Header Reference](https://tech.ebu.ch/docs/tech/tech3285.pdf)
+### Mandatory chunks
+
+- ftm - Format Chunk
+- data - Audio Data Chunk
+... chunks must appear in this order
+
+### ProTools specific chunks
+
+- minf
+- elmo
+- (data)
+- regn
+- ovwf
+- umid
+... files created by ProTools will follow this order
+
+### Optional chunks
+
+PAD - PAD chunk, fill with NUL (legacy)
+CUE - CUE chunk, defining markers and loops
+PLST - Playlist chunk
+ADTL - Associated Data List Chunk, contains text for cue points
--- wavinfo.c
+++ wavinfo.c
@@ -11,12 +11,12 @@ typedef uint32_t DWORD;
 typedef int32_t  LONG;
 
 // Chunks are the building blocks of RIFF
-// typedef struct ck
-// {
-//     DWORD ckID;             // Chunk type identifier
-//     DWORD ckSize;           // Chunk size field (size of ckData)
-//     BYTE ckData[];          // Chunk data, flexible array member
-// } ck;
+typedef struct ck
+{
+    DWORD ckID;             // Chunk type identifier
+    DWORD ckSize;           // Chunk size field (size of ckData)
+    BYTE ckData[];          // Chunk data, flexible array member
+} ck;
 
 // Format Chunk
 typedef struct fmt
@@ -49,38 +49,10 @@ typedef struct bext
     CHAR CodingHistory[];           // History coding, flexible array member
 } bext;
 
+// Prototypes of helper functions
+const char* printbyte(BYTE unprintable[], int len);
+const char* printstring(CHAR *unprintable, int len);
 
-// Helper functions
-const char* printbyte(BYTE unprintable[], int len)
-{
-    char* printable = malloc(len * sizeof(CHAR));
-    for (int i = 0; i < len; i++)
-    {
-        printable[i] = unprintable[i];
-    }
-    printable[len] = '\0';
-    return printable;
-}
-
-const char* printstring(CHAR *unprintable, int len)
-{
-    char* printable = malloc((sizeof(CHAR) + 1) * len);
-    int last_printable = 0;
-    for (int i = 0; i < len; i++)
-    {
-        printable[i] = unprintable[i];
-
-        // Track position of terminating NUL, there is one
-        if (unprintable[i] != '\0')
-        {
-            last_printable = i;
-        }
-    }
-    // Append NUL to end of printable string
-    printable[last_printable + 1] = '\0';
-
-    return printable;
-}
 
 int main(int argc, char *argv[])
 {
@@ -110,6 +82,10 @@ int main(int argc, char *argv[])
         printf("\nCould not open file: %s\n\n", filepath);
         return 1;
     }
+    else
+    {
+        printf("File name:  %s\n", filepath);
+    }
 
     // Read and check file header
 
@@ -129,7 +105,7 @@ int main(int argc, char *argv[])
     }
     printf("Chunk ID:   %s\n", groupID);
 
-    // Check fileSize, chunkSize = file length in bytes - 8
+    // Check fileSize (chunkSize = file length in bytes - 8)
     uint32_t fileSize;
     fread(&fileSize, sizeof(uint32_t), 1, audiofile);
     printf("File Size:  %u kb\n", (fileSize + 8) / 1024);
@@ -151,6 +127,25 @@ int main(int argc, char *argv[])
 
     do
     {
+        char check;
+        // Check for reaching End Of File
+        if (feof(audiofile))
+        {
+            printf("\n\n");
+            return 0;
+        }
+
+        // Ignoring NUL characters (padding)
+        fread(&check, sizeof(CHAR), 1, audiofile);
+        if (check == 0)
+        {
+            continue;
+        }
+        else
+        {
+            fseek(audiofile, -1, SEEK_CUR);
+        }
+
         // Read chunkID (4 letter code + terminator)
         char ckID[5];
         ckID[4] = '\0';
@@ -163,97 +158,155 @@ int main(int argc, char *argv[])
         // Allocate memory for chunkData
         BYTE ckData[ckSize];
 
+        // Format chunk
         if (strcmp(ckID, "fmt ") == 0)
         {
-            printf("\nFound Format Chunk with %i Bytes\n", ckSize);
-            printf("----------------------------------------\n\n");
-            // Read Format Chunk
+            printf("\n\nFound mandatory Format Chunk: %s\n", ckID);
+            printf("---------------------------------\n");
+            printf("%27u Bytes\n\n", ckSize);
+            // Read fmt chunk into fmt structure
             fmt fmtCk;
             fread(&fmtCk, sizeof(fmt), 1, audiofile);
             // Print Format Chunk Information
-            printf("          wFormatTag %i\n", fmtCk.wFormatTag);
-            printf("           nChannels %i\n", fmtCk.nChannels);
-            printf("      nSamplesPerSec %i\n", fmtCk.nSamplesPerSec);
-            printf("     nAvgBytesPerSec %i\n", fmtCk.nAvgBytesPerSec);
+            printf("          wFormatTag: %i\n", fmtCk.wFormatTag);
+            printf("           nChannels: %i\n", fmtCk.nChannels);
+            printf("      nSamplesPerSec: %i\n", fmtCk.nSamplesPerSec);
+            printf("     nAvgBytesPerSec: %i\n", fmtCk.nAvgBytesPerSec);
         }
+        // BEXT chunk
         else if (strcmp(ckID, "bext") == 0)
         {
-            printf("\nFound BEXT Chunk with %i Bytes\n", ckSize);
-            printf("----------------------------------------\n\n");
-            // Read BEXT chunk
-
+            printf("\n\nFound Broadcast Wave Chunk: %s\n", ckID);
+            printf("--------------------------------\n");
+            printf("%26u Bytes\n\n", ckSize);
+            // Read bext chunk into bext structure
             bext bextCk;
             fread(&bextCk, sizeof(bext), 1, audiofile);
-            printf("Description \n%s\n", printstring(bextCk.Description, sizeof(bextCk.Description)));
-            printf("          Originator %s\n", printstring(bextCk.Originator, sizeof(bextCk.Originator)));
-            printf(" OriginatorReference %s\n", printstring(bextCk.OriginatorReference, sizeof(bextCk.OriginatorReference)));
-            printf("     OriginationDate %s\n", printstring(bextCk.OriginationDate, sizeof(bextCk.OriginationDate)));
-            printf("     OriginationTime %s\n", printstring(bextCk.OriginationTime, sizeof(bextCk.OriginationTime)));
-            printf("    TimeReferenceLow %i\n", bextCk.TimeReferenceLow);
-            printf("   TimeReferenceHigh %i\n", bextCk.TimeReferenceHigh);
-            printf("             Version %i\n", bextCk.Version);
-            printf("                UMID %s\n", printbyte(bextCk.UMID, sizeof(bextCk.UMID)));
-            printf("       LoudnessValue %i\n", bextCk.LoudnessValue);
-            printf("       LoudnessRange %i\n", bextCk.LoudnessRange);
-            printf("    MaxTruePeakLevel %i\n", bextCk.MaxTruePeakLevel);
-            printf("MaxMomentaryLoudness %i\n", bextCk.MaxMomentaryLoudness);
-            printf("MaxShortTermLoudness %i\n", bextCk.MaxShortTermLoudness);
-            printf("            Reserved %s\n", printstring(bextCk.Reserved, sizeof(bextCk.Reserved)));
-            printf("       CodingHistory %s\n", bextCk.CodingHistory);
+            // Print BEXT Chunk Information
+            printf("Description (if any):\n%s\n\n", printstring(bextCk.Description, sizeof(bextCk.Description)));
+            printf("          Originator: %s\n", printstring(bextCk.Originator, sizeof(bextCk.Originator)));
+            printf(" OriginatorReference: %s\n", printstring(bextCk.OriginatorReference, sizeof(bextCk.OriginatorReference)));
+            printf("     OriginationDate: %s\n", printstring(bextCk.OriginationDate, sizeof(bextCk.OriginationDate)));
+            printf("     OriginationTime: %s\n", printstring(bextCk.OriginationTime, sizeof(bextCk.OriginationTime)));
+            printf("    TimeReferenceLow: %i\n", bextCk.TimeReferenceLow);
+            printf("   TimeReferenceHigh: %i\n", bextCk.TimeReferenceHigh);
+            printf("             Version: %i\n", bextCk.Version);
+            printf("                UMID: %s\n", printbyte(bextCk.UMID, sizeof(bextCk.UMID)));
+            printf("       LoudnessValue: %i\n", bextCk.LoudnessValue);
+            printf("       LoudnessRange: %i\n", bextCk.LoudnessRange);
+            printf("    MaxTruePeakLevel: %i\n", bextCk.MaxTruePeakLevel);
+            printf("MaxMomentaryLoudness: %i\n", bextCk.MaxMomentaryLoudness);
+            printf("MaxShortTermLoudness: %i\n", bextCk.MaxShortTermLoudness);
+            printf("            Reserved: %s\n", printstring(bextCk.Reserved, sizeof(bextCk.Reserved)));
+            printf("       CodingHistory: %s\n", bextCk.CodingHistory);
 
             // Rewind file pointer by 2 bytes to compensate offset:
             // Size according to ckSize = 602 vs. sizeof(bext) = 604
             fseek(audiofile, -2, SEEK_CUR);
         }
+        // Check for End of File (EOF) reached
         else if (fread(&ckData, sizeof(BYTE), ckSize, audiofile) == 0)
         {
-            printf("\nEnd of File.\n");
+            printf("\n\n");
             return 0;
         }
-        else if (strcmp(ckID, "PAD ") == 0)
-        {
-            // Origin: ???
-            printf("\nFound PAD Chunk with %i Bytes\n", ckSize);
-            printf("----------------------------------------\n");
-            printf("PAD: %s\n", ckData);
-        }
-        else if (strcmp(ckID, "minf") == 0)
-        {
-            // Origin: Pro Tools ???
-            printf("\nFound minf Chunk with %i Bytes\n", ckSize);
-            printf("----------------------------------------\n");
-            printf("minf: %s\n", ckData);
-        }
-        else if (strcmp(ckID, "elmo") == 0)
-        {
-            // Origin: Pro Tools ???
-            printf("\nFound elmo Chunk with %i Bytes\n", ckSize);
-            printf("----------------------------------------\n");
-            printf("elmo: %s\n", ckData);
-        }
+        // data chunk
         else if (strcmp(ckID, "data") == 0)
         {
-            printf("\nFound data Chunk with %i Bytes\n", ckSize);
-            printf("----------------------------------------\n");
-            printf("data: %s\n", ckData);
-        }        
+            printf("\n\nFound Data Chunk (audio samples): %s\n", ckID);
+            printf("--------------------------------------\n");
+            printf("%32u Bytes\n\n", ckSize);
+        }  
+        // ProTools chunks
+        else if ((strcmp(ckID, "minf") == 0) 
+            || (strcmp(ckID, "elmo") == 0)
+            || (strcmp(ckID, "regn") == 0)
+            || (strcmp(ckID, "ovwf") == 0)
+            || (strcmp(ckID, "umid") == 0))
+        {
+            printf("\n\nFound ProTools Chunk: %s\n", ckID);
+            printf("--------------------------\n");
+            printf("%20u Bytes\n\n", ckSize);
+            printf("Data as ASCII (if any):\n");
+            printf("%s\n", printstring(ckData, sizeof(ckData)));
+        }
+        // iXML chunk (Wave Agent / BWFXML)      
         else if (strcmp(ckID, "iXML") == 0)
         {
-            // Origin: Wave Agent / BWFXML
-            printf("\nFound iXML Chunk with %i Bytes\n", ckSize);
-            printf("----------------------------------------\n");
-
-            printf("iXML: %s\n", ckData);
+            printf("\n\nFound iXML / BWFXLM Chunk %s\n", ckID);
+            printf("------------------------------\n");
+            printf("%24u Bytes\n\n", ckSize);
+            printf("Data as ASCII (if any):\n");
+            printf("%s\n", printstring(ckData, sizeof(ckData)));
+        }
+        // PAD chunk (Found in a standard WAVE ?)
+        else if (strcmp(ckID, "PAD ") == 0)
+        {
+            printf("\n\nFound Padding Chunk: %s\n", ckID);
+            printf("-------------------------\n");
+            printf("%19u Bytes\n\n", ckSize);
+            printf("Data as ASCII (if any):\n");
+            printf("%s\n", printstring(ckData, sizeof(ckData)));
         }
         else
+        // Unknown chunk
         {
-            printf("\nFound unknown Chunk >%s< with %i Bytes\n", ckID, ckSize);
-            printf("----------------------------------------\n");
-            // Read generic chunk
-            printf("Unknown: %s\n", ckData);
+            printf("\n\nFound unknown Chunk: %s\n", ckID);
+            printf("-------------------------\n");
+            printf("%19u Bytes\n\n", ckSize);
+            printf("Data as ASCII (if any):\n");
+            printf("%s\n", printstring(ckData, sizeof(ckData)));
         }
     } while (1);
 
     // Close file, free memory
     fclose(audiofile);
 }
+
+
+// Convert unprintable binary information to printable string
+const char* printstring(CHAR *unprintable, int len)
+{
+    char* printable = malloc((sizeof(CHAR) + 1) * len);
+    int last_printable = 0;
+    for (int i = 0; i < len; i++)
+    {
+        printable[i] = unprintable[i];
+
+        // Track position of terminating NUL or whitespace, to get rid of padding
+        if (unprintable[i] != '\0' && unprintable[i] != ' ')
+        {
+            last_printable = i;
+        }
+    }
+    // Append NUL to end of printable string
+    printable[last_printable + 1] = '\0';
+
+    // Return printable of empty
+    if (strlen(printable) == 0)
+    {
+        return "... nothing here";
+    }
+    else
+    {
+        return printable;
+    }
+}
+
+
+// Convert array of UIID to printable ASCII strings
+const char* printbyte(BYTE unprintable[], int len)
+{
+    char* printable = malloc(len * sizeof(CHAR));
+    for (int i = 0; i < len; i++)
+    {
+        printable[i] = unprintable[i];
+    }
+    printable[len] = '\0';
+    return printable;
+}
+
+
+// ToDo:
+// Free all memory, check with valgrind
+// Analyse data and put up some statistics?
